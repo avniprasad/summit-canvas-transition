b15e6d679abf49ace4c64703b2bf3f7731848af2
Avni Prasad <avniprasad@gmail.com>
2024-05-30 19:08:40 -0700

    create canvas utils

---
+++ iframe_fix/utils/canvas_utils.py
@@ -0,0 +1,21 @@
+from canvasapi import Canvas
+from dotenv import load_dotenv
+from utils.csv_utils import add_edited_pages_to_tracking_csv
+
+def get_canvas_instance():
+    # Get environment variables
+    load_dotenv()
+    API_URL = os.getenv('API_URL')
+    API_KEY = os.environ.get('API_KEY')
+
+    # Create and return Canvas instance
+    return Canvas(API_URL, API_KEY)
+
+def get_html_content(course, page):
+  full_page = course.get_page(page.page_id)
+  return full_page.body
+
+def edit_page_body(page, old_body, updated_body):
+  print("Updating Page: " + page.html_url)
+  add_edited_pages_to_tracking_csv(page.page_id, page.html_url, old_body, updated_body)
+  return page.edit(wiki_page={'body': updated_body})
